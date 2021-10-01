9dc3f54ca3a7ff1d3ede69ddbebd1db0a7becf51
brainvader <brainvade@gmail.com>
2021-10-01 21:52:09 +0900

    Update static method name

--- Assets/Tests/SwapTest.cs
+++ Assets/Tests/SwapTest.cs
@@ -6,7 +6,7 @@ public class SwapTest {
     [TestCase(1, 2)]
     [TestCase(5, 10)]
     public void ReturnSwappedValues(int a, int b) {
-        var (x, y) = Swap.Swap(a, b);
+        var (x, y) = Swap.SwapTuple((a, b));
 
         Assert.That(a, Is.EqualTo(x));
         Assert.That(b, Is.EqualTo(y));
