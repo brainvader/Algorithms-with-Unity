c109266bff3d3c101199308d22554e01b2f3a477
brainvader <brainvade@gmail.com>
2021-10-01 21:40:56 +0900

    Add failing test

---
+++ Assets/Tests/SwapTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+public class SwapTest {
+    // A Test behaves as an ordinary method
+    [Test]
+    [TestCase(1, 2)]
+    [TestCase(5, 10)]
+    public void ReturnSwappedValues(int a, int b) {
+        var (x, y) = Swap.Swap(a, b);
+
+        Assert.That(a, Is.EqualTo(x));
+        Assert.That(b, Is.EqualTo(y));
+    }
+}
