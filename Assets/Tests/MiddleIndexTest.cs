7527c385e165cef7969290ec5588548f8878d333
brainvader <brainvade@gmail.com>
2021-10-02 17:14:22 +0900

    Update to pass the test

--- Assets/Tests/MiddleIndexTest.cs
+++ Assets/Tests/MiddleIndexTest.cs
@@ -5,14 +5,25 @@ public class MiddleIndexTest {
     [TestCase(1, 5, 10, 7, 12)]
     public void ReturnMidIndexInOddCase(params int[] arr) {
         int remainder = arr.Length % 2;
-        Assert.That(remainder, Is.EqualTo(0));
-        int midIndex = MiddleIndex.Middle(0, arr.Length - 1);
+        Assert.That(remainder, Is.EqualTo(1));
+
+        var expected = 2;
+        var midIndex = MiddleIndex.Middle(0, arr.Length - 1);
+        Assert.That(midIndex, Is.EqualTo(expected));
     }
 
     [Test]
     [TestCase(1, 5, 10, 7, 12, 15)]
     public void ReturnMidIndexInEvenCase(params int[] arr) {
         int remainder = arr.Length % 2;
-        Assert.That(remainder, Is.Not.EqualTo(0));
+        Assert.That(remainder, Is.EqualTo(0));
+
+        var expected = 2;
+        var midIndex = MiddleIndex.Middle(0, arr.Length - 1);
+        Assert.That(midIndex, Is.EqualTo(expected));
+
+        expected = 3;
+        midIndex = MiddleIndex.Middle(0, arr.Length);
+        Assert.That(midIndex, Is.EqualTo(expected));
     }
 }
